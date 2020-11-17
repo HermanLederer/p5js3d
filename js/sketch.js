f5363cb3f639c5c6ee777957773d55bffd0a3658
Herman Lederer <germans.lederers@gmail.com>
2020-11-17 16:08:54 +0100

    Add Vertex class

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,22 @@
+let width = 800;
+let height = 600;
+
+class Vertex {
+	constructor(x, y, z, color) {
+		this.pos = {x:x, y:y, z:z};
+		this.color = color;
+	}
+}
+
 function setup() {
-    createCanvas(400, 400); 
+	createCanvas(width, height);
 }
 
 function draw() {
-	background(0);
-}
+	background(100);
+	
+	let vert1 = new Vertex(0, 0, 0, color(255, 255, 255));
+	let vert2 = new Vertex(800, 600, 0, color(255, 255, 255));
 
+	line(vert1.pos.x, vert1.pos.y, vert2.pos.x, vert2.pos.y);
+}
